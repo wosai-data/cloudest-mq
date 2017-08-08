bb50db895fa1aff2d79e04fd5d03cad03f74873f
Dun Liu <dun@wosai-inc.com>
2017-08-08 17:53:43 +0800

    CopyDirectoryToKafka tool

--- batch/src/main/java/com/cloudest/batch/CopyDirectoryToKafka.java
+++ batch/src/main/java/com/cloudest/batch/CopyDirectoryToKafka.java
@@ -4,14 +4,21 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
+import org.apache.avro.Schema;
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.file.FileReader;
+import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.mapred.FsInput;
+import org.apache.avro.util.Utf8;
 import org.apache.commons.cli.Option;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
@@ -41,12 +48,43 @@ public class CopyDirectoryToKafka {
     private static final String OPT_KEY_COLUMN = "key-column";
     private static final String OPT_SCHEMA = "schema";
     private static final String OPT_INPUT_DIR = "input-dir";
+    private static final String OPT_ADD_COLUMN = "add-column";
+    private static final String OPT_COLUMN_VALUE = "column-value";
     
+    private static Map<Schema, Schema> schemaVariations = new HashMap<Schema, Schema>();
+
+    private static Schema copySchema(Schema sourceSchema, String extraStringColumn) {
+        String namespace = sourceSchema.getNamespace();
+        String schemaName = sourceSchema.getName() + "Copy";
+
+        Schema targetSchema = Schema.createRecord(schemaName, "", namespace, false);
+        List<Schema.Field> sourceFields = sourceSchema.getFields();
+        List<Schema.Field> targetFields = new ArrayList<Schema.Field>();
+        for (Schema.Field field: sourceFields) {
+            targetFields.add(new Schema.Field(field.name(), field.schema(), field.doc(), field.defaultVal(), field.order()));
+        }
+        if (extraStringColumn != null) {
+            targetFields.add(new Schema.Field(extraStringColumn, Schema.create(Schema.Type.STRING), "", (String)null, Schema.Field.Order.ASCENDING));
+        }
+        targetSchema.setFields(targetFields);
+
+        if (schemaVariations.containsKey(targetSchema)) {
+            targetSchema = schemaVariations.get(targetSchema);
+        } else {
+            schemaVariations.put(targetSchema, targetSchema);
+        }
+
+        return targetSchema;
+    }
+    
+
     public void run(ToolOptions options) throws Exception {
         String schemaRegistryUrl = options.get(OPT_SCHEMA_REGISTRY);
         String brokers = options.get(OPT_BROKERS);
         String topic = options.get(OPT_OUTPUT_TOPIC);
         String keyColumn = options.get(OPT_KEY_COLUMN);
+        String addColumn = options.get(OPT_ADD_COLUMN);
+        String columnValue = options.get(OPT_COLUMN_VALUE);
 
         Properties config = new Properties();
         config.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
@@ -66,16 +104,35 @@ public class CopyDirectoryToKafka {
         for (FileStatus file: files) {
             if (file.getLen() > 0) {
                 logger.info("saving file into kafka {}", file);
+
                 FsInput input = new FsInput(file.getPath(), configuration);
                 DatumReader<GenericRecord> reader = new GenericDatumReader<>();
                 FileReader<GenericRecord> fileReader = DataFileReader.openReader(input, reader);
 
-
+                Schema sourceSchema = fileReader.getSchema();
+                Schema targetSchema = copySchema(sourceSchema, addColumn);
+                
                 while(fileReader.hasNext()) {
                     GenericRecord record = fileReader.next();
-                    String key = (String)record.get(keyColumn);
+                    Object keyObject = record.get(keyColumn);
+                    if (keyObject == null) {
+                        throw new IOException(String.format("key column is null. column name %s record %s", keyColumn, record));
+                    }
+                    if ( !(keyObject instanceof Utf8) ) {
+                        throw new IOException(String.format("key column is not of type utf8. column name %s record %s", keyColumn, record));
+                    }
                     
-                    writer.write(new Record<String, GenericRecord>(key, record), new RecordWriterCallback() {
+                    String key = ((Utf8)keyObject).toString();
+                    
+                    GenericRecord target = new GenericData.Record(targetSchema);
+                    for (Schema.Field field: sourceSchema.getFields()) {
+                        target.put(field.name(), record.get(field.name()));
+                    }
+                    if (addColumn != null) {
+                        target.put(addColumn, columnValue);
+                    }
+
+                    writer.write(new Record<String, GenericRecord>(key, target), new RecordWriterCallback() {
                         @Override
                         public void onSuccess() {
                             
@@ -88,6 +145,7 @@ public class CopyDirectoryToKafka {
                     });
                 }
                 fileReader.close();
+
             }
         }
         writer.close();
@@ -129,6 +187,15 @@ public class CopyDirectoryToKafka {
                     .required()
                     .desc("the input directory").build());
 
+        options.add(Option.builder("A").hasArg().argName(OPT_ADD_COLUMN)
+                    .longOpt(OPT_ADD_COLUMN)
+                    .required(false)
+                    .desc("add a new column named the given value").build());
+
+        options.add(Option.builder("V").hasArg().argName(OPT_COLUMN_VALUE)
+                    .longOpt(OPT_COLUMN_VALUE)
+                    .required()
+                    .desc("set the value of the new column to the given value").build());
         
         if (!options.parse(args)) {
             return;
